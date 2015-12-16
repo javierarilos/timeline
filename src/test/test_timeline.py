a0a70ef1372ca7f1573ee1c7c341765fe5a2b8f1
javierarilos <javier.arilos@gmail.com>
2015-12-16 09:35:55 +0100

    Timeline obj creation

--- src/test/test_timeline.py
+++ src/test/test_timeline.py
@@ -1,6 +1,12 @@
 import unittest
 import timeline
 
+a_format = '%Y-%m-%d %H:%M:%S'
+
+def test_cb(original_datetime, *args):
+    print('called back from', original_datetime)
+
+
 class TestTimeline(unittest.TestCase):
     def setUp(self):
         pass
@@ -8,5 +14,13 @@ class TestTimeline(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_first(self):
-        self.assertTrue(3 > 2)
+    def test_timeline_creation(self):
+        timln = timeline.Timeline(test_cb, ts_col=1, ts_fmt=a_format)
+
+        self.assertEqual(timln.ts_col, 1)
+        self.assertEqual(timln.ts_fmt, a_format)
+        self.assertEqual(timln.callback, test_cb)
+
+    def test_timeline_callback_mandatory(self):
+        with self.assertRaises(Exception):
+            timln = timeline.Timeline(None)
--- src/timeline.py
+++ src/timeline.py
@@ -3,6 +3,11 @@
 """
 import sys
 
-if __name__ == '__main__':
-    print('CLI mode not yet implemented.')
-    sys.exit(1)
+
+class Timeline:
+    def __init__(self, callback, ts_col=1, ts_fmt='%Y-%m-%d %H:%M:%S'):
+        if not callback:
+            raise Exception('callback is mandatory')
+        self.callback = callback
+        self.ts_col = ts_col
+        self.ts_fmt = ts_fmt
