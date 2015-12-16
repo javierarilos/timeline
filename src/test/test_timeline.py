51def22e06a6b89cdf30c7532f2cf116eb52de10
javierarilos <javier.arilos@gmail.com>
2015-12-16 12:07:45 +0100

    event_list datetimes parsed

--- src/test/test_timeline.py
+++ src/test/test_timeline.py
@@ -1,7 +1,19 @@
 import unittest
 import timeline
+from datetime import datetime
 
 a_format = '%Y-%m-%d %H:%M:%S'
+a_timeline = [
+    ['2015-10-22 13:15:20', 'donald', 'mickey', 'supper together', 'hi there, how about tonight?'],
+    ['2015-10-22 13:15:21', 'mickey', 'pluto', 'party', 'hi there, how about tonight?'],
+    ['2015-10-22 13:15:22', 'daisy', 'minnie', 'brunch?', 'hi there, how about tonight?'],
+    ['2015-10-22 13:15:22', 'donald', 'daisy', 'drinks?', 'hi there, how about tonight?'],
+    ['2015-10-22 13:15:22', 'pluto', 'goofy', 'going to the movies', 'hi there, how about tonight'],
+    ['2015-10-22 13:15:23', 'donald', 'minnie', 'important update', 'hi there, how about tonight?'],
+    ['2015-10-22 13:15:23', 'donald', 'goofy', 'football on sunday', 'hi there, how about tonight'],
+    ['2015-10-22 13:15:24', 'goofy', 'micky', 'TIMELINErestriction', 'ts must be sorted, old 2 new']
+]
+
 
 def test_cb(original_datetime, *args):
     print('called back from', original_datetime)
@@ -24,3 +36,50 @@ class TestTimeline(unittest.TestCase):
     def test_timeline_callback_mandatory(self):
         with self.assertRaises(Exception):
             timln = timeline.Timeline(None)
+
+    def test_validate_ko_non_iterable(self):
+        timln = timeline.Timeline(test_cb, ts_col=0, ts_fmt=a_format)
+
+        is_valid = timln.is_valid(234)
+
+        self.assertFalse(is_valid)
+
+    def test_validate_ok(self):
+        timln = timeline.Timeline(test_cb, ts_col=0, ts_fmt=a_format)
+
+        is_valid = timln.is_valid(a_timeline)
+
+        self.assertTrue(is_valid)
+
+    def test_parse_ok(self):
+        timln = timeline.Timeline(test_cb, ts_col=0, ts_fmt=a_format)
+
+        test_timeline = [
+            ['2015-10-22 13:15:20', 'donald', 'mickey'],
+            ['2015-10-22 13:15:21', 'mickey', 'pluto', 'yay']
+        ]
+
+        parsed_timeline = timln.parse(test_timeline)
+
+        for parsed_row, original_row in zip(parsed_timeline, test_timeline):
+            parsed_datetime = parsed_row[0]
+            self.assertIsInstance(parsed_datetime, datetime)
+            self.assertEqual(parsed_datetime.year, 2015)
+            self.assertEqual(parsed_datetime.month, 10)
+            self.assertEqual(parsed_datetime.day, 22)
+            self.assertEqual(parsed_datetime.hour, 13)
+            self.assertEqual(parsed_datetime.minute, 15)
+
+            self.assertEqual(len(parsed_row), len(original_row)+1)
+            self.assertEqual(parsed_row[1:], original_row)
+
+    def test_parse_ko_bad_dateformat(self):
+        timln = timeline.Timeline(test_cb, ts_col=0, ts_fmt=a_format)
+
+        test_timeline = [
+            ['BADDATEFORMAT', 'donald', 'mickey'],
+            ['2015-10-22 13:15:21', 'mickey', 'pluto', 'yay']
+        ]
+
+        with self.assertRaises(ValueError):
+            parsed_timeline = timln.parse(test_timeline)
--- src/timeline.py
+++ src/timeline.py
@@ -2,12 +2,42 @@
 """ Timeline
 """
 import sys
+from datetime import datetime
 
 
 class Timeline:
-    def __init__(self, callback, ts_col=1, ts_fmt='%Y-%m-%d %H:%M:%S'):
+    def __init__(self, callback, ts_col=0, ts_fmt='%Y-%m-%d %H:%M:%S'):
         if not callback:
             raise Exception('callback is mandatory')
         self.callback = callback
         self.ts_col = ts_col
         self.ts_fmt = ts_fmt
+        self.events = []
+
+    def parse(self, event_list):
+        """Parses event_list[:][ts_col] to datetime using ts_fmt
+
+        Returns a new list of rows (lists) where
+            row first column is the parsed datetime, the rest is the original row
+        """
+        return list(map(lambda event: [datetime.strptime(event[0], self.ts_fmt)] + event, event_list))
+
+    def is_valid(self, event_list, skip_first=False):
+        """Validates event_list, returns True on valid list.
+        - event_list is expected to: contain lists or tuples with a parseable datetime
+            in column self.ts_col using self.ts_fmt
+            events in event_list must be sorted by date from old to new.
+        - skip_first indicates if first row should be skipped (eg: contains headers)
+        """
+        if not isinstance(event_list, list):
+            return False
+        event_list_parsed = self.parse(event_list)
+        return True
+
+    def start(self, event_list, skip_first=False):
+        """Starts calling back self.callback following the timeline specified by event_list.
+
+        event_list is expected to be valid
+        """
+        parsed_event_list = self.parse(event_list)
+        self.validate(event_list, skip_first=skip_first)
